8957f6956567d42440dab5e5fb327b701e1fea0c
Akintunde Owojori <105000665+oneboyfromife@users.noreply.github.com>
2022-05-05 16:32:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let outputScreen = document.getElementById('output-screen')
+
+function display(num) {
+    outputScreen.value += num;
+}
+function Calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    } 
+    catch (err) 
+    {
+        alert('Invalid Input')
+    }
+}
+function Clear() {
+    outputScreen.value = ''
+}
+function del() {
+    outputScreen.value = outputScreen.value.slice(0,-1)
+}
+
+const audio = new Audio();
+    audio.src = 'click.mp3';
